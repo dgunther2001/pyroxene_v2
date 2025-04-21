0cd1a514b50af10839a01ee99a28760f23dafff5
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2025-04-20 21:35:27 -0400

    Added Logger Foundry as a submodule and move to a separate repo

--- logger_foundry/ci_tests/build_test_foundry.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-def main():
-    os.environ["LOGGER_FOUNDRY_BUILD_TESTING_PATH"] = os.path.join(os.getcwd(), "logger_foundry_lib")
-    subprocess.run(["python3", "build_logger_foundry.py", "--cmake-prefix", os.environ["LOGGER_FOUNDRY_BUILD_TESTING_PATH"]], cwd="..")
-
-main()
